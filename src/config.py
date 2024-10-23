1dc1b1fe87290e4e34c76d8a4f16db32683ca6d2
Alberto Becerra <abecerr1@bsc.es>
2024-10-23 20:55:23 +0200

    include default task path

--- src/config.py
+++ src/config.py
@@ -2,6 +2,7 @@
 
 POSTS_PATH = "data/complete_data/posts.csv"
 FACT_CHECKS_PATH = "data/complete_data/fact_checks.csv"
+TASKS_PATH = "data/splits/tasks_no_gs_overlap.json"
 GS_PATH = "data/complete_data/pairs.csv"
 PHASE1_TASKS_PATH = "data/complete_data/tasks.json"
 OUTPUT_PATH = "output"
